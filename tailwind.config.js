64891833478cb77a62ae502c10e710de9fdccc7f
Akshar Kumar Talla <159448336+aksharkt@users.noreply.github.com>
2025-07-24 16:30:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+// 1. Import the default theme
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    // 2. Add the extend block with the new font family
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
